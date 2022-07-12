7d413bfe9a2493f1b2a027f9cffc86becb9ef2a2
sheepluo <sheepluo@tencent.com>
2022-07-12 23:26:08 +0800

    fix: package.json

--- packages/server/services/execute.ts
+++ packages/server/services/execute.ts
@@ -6,6 +6,7 @@ const exec = util.promisify(require('child_process').exec);
 const scriptsPath = path.resolve('packages/scripts');
 
 export default async function execScript(params: BaseObject) {
+    // TODO: 语种参数（zh/en），是否使用 useVModel 
     const script = `node ${scriptsPath}/download.js && node ${scriptsPath}/index.js ${params.component} '${params.platform}'`;
     const { stdout, stderr } = await exec(script);
     console.log('Execute:', script);
