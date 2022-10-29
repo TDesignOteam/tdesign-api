d0ad40887c3e156478ecce8144d60f7bd2aca18e
sheepluo <sheepluo@tencent.com>
2022-10-29 15:13:13 +0800

    feat: update input value

---
+++ packages/products/tdesign-react/src/input/defaultProps.ts
@@ -0,0 +1,20 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdInputProps } from './type';
+
+export const inputDefaultProps: TdInputProps = {
+  align: 'left',
+  allowInputOverMax: false,
+  autoWidth: false,
+  autocomplete: undefined,
+  autofocus: false,
+  clearable: false,
+  placeholder: undefined,
+  readonly: false,
+  showClearIconOnEmpty: false,
+  showLimitNumber: false,
+  size: 'medium',
+  type: 'text',
+};
