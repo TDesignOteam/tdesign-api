4058feede3e44435d8ac1d26a80fa39d637056da
sheepluo <sheepluo@tencent.com>
2022-11-19 18:15:43 +0800

    feat: add allowInputOverLimit

---
+++ packages/products/tdesign-react/src/input-number/defaultProps.ts
@@ -0,0 +1,18 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdInputNumberProps } from './type';
+
+export const inputNumberDefaultProps: TdInputNumberProps = {
+  autoWidth: false,
+  decimalPlaces: undefined,
+  largeNumber: false,
+  max: Infinity,
+  min: -Infinity,
+  placeholder: undefined,
+  readonly: false,
+  size: 'medium',
+  step: 1,
+  theme: 'row',
+};
