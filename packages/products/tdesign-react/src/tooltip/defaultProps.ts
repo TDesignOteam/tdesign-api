8e1db8351317d71a496aa10a09f4ed3205c4f732
carolin913@126.com <lincao@tencent.com>
2022-11-25 17:55:40 +0800

    feat(): tooltiplite placement

--- packages/products/tdesign-react/src/tooltip/defaultProps.ts
+++ packages/products/tdesign-react/src/tooltip/defaultProps.ts
@@ -2,18 +2,11 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdTooltipProps, TdTooltipLiteProps } from './type';
-
-export const tooltipDefaultProps: TdTooltipProps = {
-  destroyOnClose: true,
-  placement: 'top',
-  showArrow: true,
-  theme: 'default',
-};
+import { TdTooltipLiteProps } from './type';
 
 export const tooltipLiteDefaultProps: TdTooltipLiteProps = {
   placement: 'top',
   showArrow: true,
-  theme: 'dark',
-  trigger: 'hover',
+  showShadow: true,
+  theme: 'default',
 };
