7705a7efefbba04478aea5d5a270cb624c50e8c0
sheepluo <sheepluo@tencent.com>
2022-11-27 17:31:26 +0800

    feat: update table

--- packages/products/tdesign-react/src/table/defaultProps.ts
+++ packages/products/tdesign-react/src/table/defaultProps.ts
@@ -2,27 +2,7 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdBaseTableProps, TdPrimaryTableProps } from './type';
-
-export const baseTableDefaultProps: TdBaseTableProps = {
-  bordered: false,
-  columns: [],
-  data: [],
-  disableDataPage: false,
-  empty: '',
-  footData: [],
-  footerAffixedBottom: false,
-  headerAffixedTop: false,
-  hover: false,
-  loading: undefined,
-  resizable: false,
-  rowKey: 'id',
-  showHeader: true,
-  size: 'medium',
-  stripe: false,
-  tableLayout: 'fixed',
-  verticalAlign: 'middle',
-};
+import { TdPrimaryTableProps } from './type';
 
 export const primaryTableDefaultProps: Pick<
   TdPrimaryTableProps,
