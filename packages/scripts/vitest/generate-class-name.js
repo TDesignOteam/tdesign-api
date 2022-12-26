ef53b67fff4eedf515cec997ce4cea200c3796f3
sheepluo <sheepluo@tencent.com>
2022-12-26 12:33:19 +0800

    feat: add snapshot

--- packages/scripts/vitest/generate-class-name.js
+++ packages/scripts/vitest/generate-class-name.js
@@ -3,19 +3,20 @@
  */
 function generateClassNameUnitCase(test, oneApiData, framework, component) {
   if (['VueNext(PC)', 'Vue(PC)'].includes(framework)) {
-    return generateVue3ClassName(test, oneApiData, component);
+    return generateVue3ClassName(test, oneApiData, framework, component);
   }
 }
 
-function generateVue3ClassName(test, oneApiData, component) {
+function generateVue3ClassName(test, oneApiData, framework, component) {
   const { className, snapshot } = test;
+  const componentCode = `<${component} ${oneApiData.field_name}={item}>Text</${component}>`;
   // 不同的值控制不同的类名（一个值必定对应着一个类名）
   if (typeof className === 'string' && className.indexOf('${item}') != -1 && oneApiData.field_type_text[0] === 'String') {
     const enums = oneApiData.field_enum.split('/');
     const arr = [
       `[${enums.map(val => `'${val}'`).join(', ')}].forEach((item) => {`,
       `it(\`props.${oneApiData.field_name} is equal to \${ item }\`, () => {`,
-      `const wrapper = mount(<${component} ${oneApiData.field_name}={item}>Text</${component}>);`,
+      `const wrapper = ${getMountCode(framework, componentCode)};`,
       `expect(wrapper.classes(\`${className}\`)).toBeTruthy();`,
       snapshot ? `expect(wrapper.element).toMatchSnapshot();` : '',
       `});`,
@@ -32,7 +33,7 @@ function generateVue3ClassName(test, oneApiData, component) {
       `const ${classNameVariable} = ${getArrayCode(className)};`,
       `${getArrayCode(enums)}.forEach((item, index) => {`,
       `it(\`props.${oneApiData.field_name} is equal to \${ item }\`, () => {`,
-      `const wrapper = mount(<${component} ${oneApiData.field_name}={item}>Text</${component}>);`,
+      `const wrapper = ${getMountCode(framework, componentCode)};`,
       `expect(wrapper.classes(${classNameVariable}[index])).toBeTruthy();`,
       snapshot ? `expect(wrapper.element).toMatchSnapshot();` : '',
       `});`,
@@ -53,6 +54,23 @@ function generateVue3ClassName(test, oneApiData, component) {
       '});',
     ];
     return arr.filter(v => v).join('\n');
+  }
+}
+
+function getMountCode(framework, componentCode) {
+  if (framework === 'Vue(PC)') {
+    return [
+      `mount({
+        render() {
+          return (
+            ${componentCode}
+          );
+        }
+      })`
+    ];
+  }
+  if (framework === 'VueNext(PC)') {
+    return `mount(${componentCode})`;
   }
 }
 
