5373726d79d77a6522d33ebb04ec7a182b8f1b0e
sheepluo <sheepluo@tencent.com>
2022-12-26 19:51:42 +0800

    feat: support attribute unit test case for react

--- packages/scripts/vitest/REAMD.md
+++ packages/scripts/vitest/REAMD.md
@@ -1,19 +1,47 @@
+##
+
+```bash
+npm run api:docs Button 'VueNext(PC)'  vitest,finalProject
+npm run api:docs Button 'Vue(PC)'  vitest,finalProject
+npm run api:docs Button 'React(PC)'  vitest,finalProject
+```
+
 ## 测试类名
+
+1. 检测是否存在某个类名，比如：button.block
 
 ```json
 {"PC":{"className":"t-size-full-width"}}
 ```
 
+2. 检测某个 API 类名和属性值是否相同，且存在，比如 button.variant
 ```json
 {"PC":{"className": "t-button--variant-${item}", "snapshot": true }}
 ```
 
+3. 检测某个 API 不同的值对应完全不同的类名，比如：button.size 和 button.shape
 ```json
-{"PC":{"className": ["", "t-button--shape-square", "t-button--shape-round", "t-button--shape-circle" ]}}
+{"PC":{"className": [{ "t-button--shape-square": false }, "t-button--shape-square", "t-button--shape-round", "t-button--shape-circle" ]}}
 ```
 
 ## 测试属性
 
+1. 检测某个属性的枚举值值是否正确，比如：button.type
+
 ```json
 {"PC":{"attribute": { "type": ["submit", "reset", "button"] }}}
 ```
+
+2. 检测某个 API 的属性是否允许直接透传，比如：button.href
+
+```json
+{"PC":{"attribute": { "href": "https://tdesign.tencent.com/" }, "snapshot": true }}
+```
+
+## 测试自定义元素 TNode
+
+```json
+{"PC": { "tnode": true, "snapshot": true }}
+```
+
+其中，snapshot: true 表示是否生成当前测试用例快照
--- packages/scripts/vitest/generate-attribute.js
+++ packages/scripts/vitest/generate-attribute.js
@@ -5,7 +5,7 @@ function generateAttributeUnitCase(test, oneApiData, framework, component) {
     return generateVueAttribute(test, oneApiData, framework, component);
   }
   if (['React(PC)'].includes(framework)) {
-    // return generateReactAttribute(test, oneApiData, component);
+    return generateReactAttribute(test, oneApiData, component);
   }
 }
 
@@ -38,6 +38,40 @@ function generateVueAttribute(test, oneApiData, framework, component) {
         const wrapper = ${getMountCode(framework, componentCode)};
         expect(wrapper.attributes('${attributeName}')).toBe('${attributeValue}');`,
         snapshot ? 'expect(wrapper.element).toMatchSnapshot();' : '',
+      `})`,
+    ];
+    return arr.filter(v => v);
+  }
+}
+
+function generateReactAttribute(test, oneApiData, component) {
+  const { attribute, snapshot } = test;
+  const attributeName = Object.keys(attribute)[0];
+  // 期望的属性值
+  const attributeValue = attribute[attributeName];
+  // 属性全部枚举值
+  const propsValues = oneApiData.field_enum?.split('/');
+  if (Array.isArray(attributeValue) && propsValues.length) {
+    const arr = [
+      `const attributeValues = ${getArrayCode(attributeValue)};`,
+      `${getArrayCode(propsValues)}.forEach((item, index) => {`,
+        `it(\`props.${oneApiData.field_name} is equal to \${item}\`, () => {
+          const { container } = render(<${component} ${oneApiData.field_name}={item}></${component}>);
+          expect(container.firstChild.getAttribute('${attributeName}')).toBe(attributeValues[index]);`,
+          snapshot ? 'expect(container).toMatchSnapshot();' : '',
+        `});`,
+      `});`,
+    ];
+    return arr.filter(v => v);
+  }
+  // 测试属性赋值，如：<Button href="https://tdesign.tencent.com/" />
+  if (typeof attributeValue === 'string') {
+    const componentCode = `<${component} ${oneApiData.field_name}='${attributeValue}'></${component}>`;
+    const arr = [
+      `it(${getItDescription(oneApiData)}, () => {
+        const { container } = render(${componentCode});
+        expect(container.firstChild.getAttribute('${attributeName}')).toBe('${attributeValue}');`,
+        snapshot ? 'expect(container).toMatchSnapshot();' : '',
       `})`,
     ];
     return arr.filter(v => v);
