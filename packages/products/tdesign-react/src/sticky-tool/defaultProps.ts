a2c42ef428ea17e9a93f656feee2a9c8d85d18fe
yuyang <uyarnchen@gmail.com>
2023-02-22 17:16:48 +0800

    feat: complete sticky tool api (#81)

---
+++ packages/products/tdesign-react/src/sticky-tool/defaultProps.ts
@@ -0,0 +1,14 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdStickyToolProps, TdStickyItemProps } from './type';
+
+export const stickyToolDefaultProps: TdStickyToolProps = {
+  list: [],
+  placement: 'right-bottom',
+  shape: 'square',
+  type: 'normal',
+};
+
+export const stickyItemDefaultProps: TdStickyItemProps = { trigger: 'hover' };
