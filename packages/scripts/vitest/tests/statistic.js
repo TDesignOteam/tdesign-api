c997ea642eaa762e15b3eeee30655b9da408f085
xiaosansiji <xiaosansiji@gmail.com>
2023-04-20 19:46:46 +0800

    feat: add Statistic component

---
+++ packages/scripts/vitest/tests/statistic.js
@@ -0,0 +1,19 @@
+module.exports = {
+  Statistic: {
+    animation_3457: { field_name: 'animation', id: 3457 },
+    animationStart_3455: { field_name: 'animationStart', id: 3455 },
+    color_3459: { field_name: 'color', id: 3459 },
+    decimalPlaces_3453: { field_name: 'decimalPlaces', id: 3453 },
+    extra_3462: { field_name: 'extra', id: 3462 },
+    format_3452: { field_name: 'format', id: 3452 },
+    loading_3456: { field_name: 'loading', id: 3456 },
+    prefix_3463: { field_name: 'prefix', id: 3463 },
+    separator_3454: { field_name: 'separator', id: 3454 },
+    suffix_3464: { field_name: 'suffix', id: 3464 },
+    title_3450: { field_name: 'title', id: 3450 },
+    trend_3460: { field_name: 'trend', id: 3460 },
+    trendPlacement_3461: { field_name: 'trendPlacement', id: 3461 },
+    unit_3465: { field_name: 'unit', id: 3465 },
+    value_3451: { field_name: 'value', id: 3451 },
+  },
+};
