dfdc52170d099e0eb02d77449923af05a702bdc6
chaishi <974383157@qq.com>
2023-05-31 22:37:46 +0800

    feat: update

--- packages/products/tdesign-mobile-react/src/common.ts
+++ packages/products/tdesign-mobile-react/src/common.ts
@@ -5,7 +5,7 @@ import { ReactElement, ReactNode, CSSProperties, FormEvent, DragEvent, Synthetic
 // TElement 表示 API 只接受传入组件
 export type TElement<T = undefined> = T extends undefined ? ReactElement : (props: T) => ReactElement;
 // 1. TNode = ReactNode; 2. TNode<T> = (props: T) => ReactNode
-export type TNode<T = undefined> = T extends undefined ? ReactNode : (props: T) => ReactNode;
+export type TNode<T = undefined> = T extends undefined ? ReactNode : (ReactNode | ((props: T) => ReactNode));
 
 export type AttachNodeReturnValue = HTMLElement | Element | Document;
 export type AttachNode = CSSSelector | ((triggerNode?: HTMLElement) => AttachNodeReturnValue);
@@ -54,6 +54,8 @@ export type TreeOptionData<T = string | number> = {
   text?: string;
   /** option value */
   value?: T;
+  /** option node content */
+  content?: string | TNode;
 } & PlainObject;
 
 export type SizeEnum = 'small' | 'medium' | 'large';
@@ -105,3 +107,13 @@ export interface TScroll {
  * @deprecated use TScroll instead
  */
 export type InfinityScroll = TScroll;
+
+export interface ScrollToElementParams {
+  /** 跳转元素下标 */
+  index: number;
+  /** 跳转元素距离顶部的距离 */
+  top?: number;
+  /** 单个元素高度非固定场景下，即 isFixedRowHeight = false。延迟设置元素位置，一般用于依赖不同高度异步渲染等场景，单位：毫秒 */
+  time?: number;
+  behavior?: 'auto' | 'smooth';
+}
