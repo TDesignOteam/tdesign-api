bc8a796c23c16271f56d01b07fb71dc44bcaa920
Uyarn <uyarnchen@gmail.com>
2023-07-24 16:00:18 +0800

    feat(transfer): sort target list by dragging

---
+++ packages/products/tdesign-react/src/transfer/defaultProps.ts
@@ -0,0 +1,19 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdTransferProps } from './type';
+
+export const transferDefaultProps: TdTransferProps = {
+  checked: [],
+  data: [],
+  direction: 'both',
+  empty: '',
+  search: false,
+  showCheckAll: true,
+  targetDraggable: false,
+  targetSort: 'original',
+  title: [],
+  tree: '传入 Tree 组件定义树形结构',
+  defaultValue: [],
+};
