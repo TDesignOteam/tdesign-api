81d2b1f9931e3658e27e1b0a1fde1291124cf847
wū yāng <uyarnchen@gmail.com>
2024-03-12 21:10:20 +0800

    feat: breadcrumb item support click event (#238)

--- packages/products/tdesign-react/src/breadcrumb/defaultProps.ts
+++ packages/products/tdesign-react/src/breadcrumb/defaultProps.ts
@@ -2,7 +2,9 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdBreadcrumbItemProps } from './type';
+import { TdBreadcrumbProps, TdBreadcrumbItemProps } from './type';
+
+export const breadcrumbDefaultProps: TdBreadcrumbProps = { maxItemWidth: undefined };
 
 export const breadcrumbItemDefaultProps: TdBreadcrumbItemProps = {
   maxWidth: undefined,
