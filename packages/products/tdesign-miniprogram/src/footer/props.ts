e9b272c2a432f89f3b25624759ea10cc54d08e9c
黎伟杰 <674416404@qq.com>
2024-04-08 15:56:22 +0800

    chore: npm run api:docs Footer "Miniprogram"

--- packages/products/tdesign-miniprogram/src/footer/props.ts
+++ packages/products/tdesign-miniprogram/src/footer/props.ts
@@ -6,12 +6,17 @@
 
 import { TdFooterProps } from './type';
 const props: TdFooterProps = {
-  /** 版权信息，type 为`text`生效 */
+  /** 已废弃。版权信息，type 为`text`生效 */
   copyright: {
     type: String,
     value: '',
   },
-  /** 图标配置，type 为`logo`生效。`logo.icon` 表示图标链接地址，`logo.title` 表示标题文本，`logo.url` 表示链接跳转地址 */
+  /** 链接列表。name 表示链接名称， url 表示链接 page 路径，目前只支持小程序内部跳转，openType 表示跳转方式 */
+  links: {
+    type: Array,
+    value: [],
+  },
+  /** 图标配置。`logo.icon` 表示图标链接地址，`logo.title` 表示标题文本，`logo.url` 表示链接跳转地址 */
   logo: {
     type: Object,
   },
@@ -20,12 +25,17 @@ const props: TdFooterProps = {
     type: String,
     value: '',
   },
-  /** 链接列表，type 为`text`生效。name 表示链接名称， url 表示链接 page 路径，目前只支持小程序内部跳转，openType 表示跳转方式 */
+  /** 版权信息 */
+  text: {
+    type: String,
+    value: '',
+  },
+  /** 已废弃。链接列表，type 为`text`生效。name 表示链接名称， url 表示链接 page 路径，目前只支持小程序内部跳转，openType 表示跳转方式 */
   textLinkList: {
     type: Array,
     value: [],
   },
-  /** 页脚展示类型 */
+  /** 已废弃。页脚展示类型 */
   theme: {
     type: String,
     value: 'text',
