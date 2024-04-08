f6b4c4de9eb35bbb654712a5864d3fd79fa9b976
黎伟杰 <674416404@qq.com>
2024-04-08 21:41:48 +0800

    feat: update tag api

--- packages/products/tdesign-miniprogram/src/tag/README.md
+++ packages/products/tdesign-miniprogram/src/tag/README.md
@@ -13,7 +13,7 @@ disabled | Boolean | false | 标签禁用态，失效标签不能触发事件。
 icon | String / Object / Slot | - | 标签中的图标，可自定义图标呈现。[通用类型定义](https://github.com/Tencent/tdesign-miniprogram/blob/develop/src/common/common.ts) | N
 max-width | String / Number | - | 标签最大宽度，宽度超出后会出现省略号。示例：'50px' / 80 | N
 shape | String | square | 标签类型，有三种：方形、圆角方形、标记型。可选项：square/round/mark | N
-size | String | medium | 标签尺寸。可选项：small/medium/large/extra-large。TS 类型：`SizeEnum`。[通用类型定义](https://github.com/Tencent/tdesign-miniprogram/blob/develop/src/common/common.ts) | N
+size | String | medium | 标签尺寸。可选项：small/medium/large/extra-large | N
 theme | String | default | 组件风格，用于描述组件不同的应用场景。可选项：default/primary/warning/danger/success | N
 variant | String | dark | 标签风格变体。可选项：dark/light/outline/light-outline | N
 
@@ -58,13 +58,3 @@ close | \- | 如果关闭按钮存在，点击关闭按钮时触发
 类名 | 描述
 -- | --
 t-class | 根节点样式类
-
-
-### CheckTagGroup Props
-
-名称 | 类型 | 默认值 | 描述 | 必传
--- | -- | -- | -- | --
-style | Object | - | 样式 | N
-custom-style | Object | - | 样式，一般用于开启虚拟化组件节点场景 | N
-checked-props | Object | - | 透传标签选中态属性。TS 类型：`TdTagProps` | N
-unchecked-props | Object | - | 透传标签未选态属性。TS 类型：`TdTagProps` | N
---
+++ packages/products/tdesign-mobile-vue/src/tag/check-tag-props.ts
@@ -0,0 +1,74 @@
+/* eslint-disable */
+
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdCheckTagProps } from '../tag/type';
+import { PropType } from 'vue';
+
+export default {
+  /** 标签选中的状态，默认风格（theme=default）才有选中态 */
+  checked: {
+    type: Boolean,
+    default: undefined,
+  },
+  modelValue: {
+    type: Boolean,
+    default: undefined,
+  },
+  /** 标签选中的状态，默认风格（theme=default）才有选中态，非受控属性 */
+  defaultChecked: {
+    type: Boolean,
+    default: undefined,
+  },
+  /** 标签是否可关闭 */
+  closable: Boolean,
+  /** 组件子元素；传入数组时：[选中内容，非选中内容] */
+  content: {
+    type: [String, Number, Array, Function] as PropType<TdCheckTagProps['content']>,
+  },
+  /** 组件子元素，默认插槽 */
+  default: {
+    type: [String, Function] as PropType<TdCheckTagProps['default']>,
+  },
+  /** 标签禁用态，失效标签不能触发事件。默认风格（theme=default）才有禁用态 */
+  disabled: Boolean,
+  /** 标签中的图标，可自定义图标呈现 */
+  icon: {
+    type: Function as PropType<TdCheckTagProps['icon']>,
+  },
+  /** 标签类型，有三种：方形、圆角方形、标记型 */
+  shape: {
+    type: String as PropType<TdCheckTagProps['shape']>,
+    default: 'square' as TdCheckTagProps['shape'],
+    validator(val: TdCheckTagProps['shape']): boolean {
+      if (!val) return true;
+      return ['square', 'round', 'mark'].includes(val);
+    },
+  },
+  /** 标签尺寸 */
+  size: {
+    type: String as PropType<TdCheckTagProps['size']>,
+    default: 'medium' as TdCheckTagProps['size'],
+    validator(val: TdCheckTagProps['size']): boolean {
+      if (!val) return true;
+      return ['small', 'medium', 'large'].includes(val);
+    },
+  },
+  /** 标签风格变体 */
+  variant: {
+    type: String as PropType<TdCheckTagProps['variant']>,
+    default: 'dark' as TdCheckTagProps['variant'],
+    validator(val: TdCheckTagProps['variant']): boolean {
+      if (!val) return true;
+      return ['dark', 'light', 'outline', 'light-outline'].includes(val);
+    },
+  },
+  /** 状态切换时触发 */
+  onChange: Function as PropType<TdCheckTagProps['onChange']>,
+  /** 点击标签时触发 */
+  onClick: Function as PropType<TdCheckTagProps['onClick']>,
+  /** 如果关闭按钮存在，点击关闭按钮时触发 */
+  onClose: Function as PropType<TdCheckTagProps['onClose']>,
+};
