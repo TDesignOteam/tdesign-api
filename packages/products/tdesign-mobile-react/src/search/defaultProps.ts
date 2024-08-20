0bcdff236bbad3e3de5e7ca72c06d8405435d8ff
slatejack <39983389+slatejack@users.noreply.github.com>
2024-08-20 20:30:24 +0800

    fix(Search): remove duplicate type in react mobile (#378)

---
+++ packages/products/tdesign-mobile-react/src/search/defaultProps.ts
@@ -0,0 +1,17 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdSearchProps } from './type';
+
+export const searchDefaultProps: TdSearchProps = {
+  action: '',
+  center: false,
+  clearable: true,
+  disabled: false,
+  focus: false,
+  leftIcon: 'search',
+  placeholder: '',
+  readonly: false,
+  shape: 'square',
+};
