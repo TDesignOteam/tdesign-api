162dda4b21b20e39fe435c937b6a972fb7add2f4
Uyarn <uyarnchen@gmail.com>
2024-08-21 17:11:37 +0800

    feat(empty): add size API

--- packages/products/tdesign-react/src/empty/type.ts
+++ packages/products/tdesign-react/src/empty/type.ts
@@ -5,11 +5,11 @@
  * */
 
 import { ImageProps } from '../image';
-import { TNode, TElement, Styles } from '../common';
+import { TNode, TElement, SizeEnum, Styles } from '../common';
 
 export interface TdEmptyProps {
   /**
-   * 操作区域
+   * 操作按钮
    */
   action?: TElement;
   /**
@@ -24,6 +24,11 @@ export interface TdEmptyProps {
    * 透传图片样式表
    */
   imageStyle?: Styles;
+  /**
+   * 空状态的尺寸，默认为 `medium`
+   * @default medium
+   */
+  size?: SizeEnum;
   /**
    * 错误标题
    */
