bf7fa57c9f608becb5535acf32d65cf089c18915
toby <toviyyahxu@tencent.com>
2024-08-26 20:19:14 +0800

    feat(SideBar): mobile-react创建SideBar组件

---
+++ packages/scripts/vitest/tests/side-bar.js
@@ -0,0 +1,19 @@
+module.exports = {
+  SideBar: {
+    children_1724669424: { field_name: 'children', id: 1724669424 },
+    defaultValue_1711973521: { field_name: 'defaultValue', id: 1711973521 },
+    value_3379: { field_name: 'value', id: 3379 },
+    value_3532: { field_name: 'value', id: 3532 },
+    change_3380: { field_name: 'change', id: 3380 },
+    change_1712580336: { field_name: 'change', id: 1712580336 },
+    click_3381: { field_name: 'click', id: 3381 },
+  },
+  SideBarItem: {
+    badgeProps_3533: { field_name: 'badgeProps', id: 3533 },
+    disabled_3536: { field_name: 'disabled', id: 3536 },
+    icon_3534: { field_name: 'icon', id: 3534 },
+    icon_1712580374: { field_name: 'icon', id: 1712580374 },
+    label_3535: { field_name: 'label', id: 3535 },
+    value_3515: { field_name: 'value', id: 3515 },
+  },
+};
