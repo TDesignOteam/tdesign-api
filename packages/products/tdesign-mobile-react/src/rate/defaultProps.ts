edbf833e12efd8e65f591c4968e550e774ff8be1
ming680 <827668037@qq.com>
2024-08-26 13:20:27 +0800

    feat(rate): 修改 mobile react rate 组件 api (#390)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ packages/products/tdesign-mobile-react/src/rate/defaultProps.ts
@@ -0,0 +1,18 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdRateProps } from './type';
+
+export const rateDefaultProps: TdRateProps = {
+  allowHalf: false,
+  color: '#ED7B2F',
+  count: 5,
+  disabled: undefined,
+  gap: 8,
+  placement: 'top',
+  showText: false,
+  size: '24px',
+  texts: [],
+  defaultValue: 0,
+};
