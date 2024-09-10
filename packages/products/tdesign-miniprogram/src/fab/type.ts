5eb9c2f0d72c2f2667597066498a8ba8c644c13f
Y. <anly_yaw@163.com>
2024-09-10 15:16:46 +0800

    feat(Fab): add y-bounds props (#408)
    * feat(Fab): add y-edge props
    
    * fix(Fab): fix cr
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/products/tdesign-miniprogram/src/fab/type.ts
+++ packages/products/tdesign-miniprogram/src/fab/type.ts
@@ -15,12 +15,12 @@ export interface TdFabProps {
     value?: ButtonProps;
   };
   /**
-   * `true` / `'all'`可拖动<br>`'vertical'`可垂直拖动<br>`'horizontal'`可水平拖动<br>`false`禁止拖动
+   * 是否可拖拽。`true` / `'all'`可拖动<br>`'vertical'`可垂直拖动<br>`'horizontal'`可水平拖动<br>`false`禁止拖动
    * @default false
    */
   draggable?: {
     type: null;
-    value?: string | boolean;
+    value?: boolean | FabDirectionEnum;
   };
   /**
    * 图标
@@ -54,4 +54,13 @@ export interface TdFabProps {
     type: BooleanConstructor;
     value?: boolean;
   };
+  /**
+   * 设置垂直方向边界限制，示例：[48, 48] 或 ['96rpx', 80]
+   */
+  yBounds?: {
+    type: ArrayConstructor;
+    value?: Array<string | number>;
+  };
 }
+
+export type FabDirectionEnum = 'all' | 'vertical' | 'horizontal';
