f4be235e663bad3ad89b0430298433671e3bc912
黎伟杰 <674416404@qq.com>
2024-09-11 12:06:48 +0800

    feat: update api

--- packages/products/tdesign-mobile-react/src/checkbox/defaultProps.ts
+++ packages/products/tdesign-mobile-react/src/checkbox/defaultProps.ts
@@ -2,7 +2,21 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdCheckboxGroupProps } from './type';
+import { TdCheckboxProps, TdCheckboxGroupProps } from './type';
+
+export const checkboxDefaultProps: TdCheckboxProps = {
+  block: true,
+  borderless: false,
+  checkAll: false,
+  defaultChecked: false,
+  disabled: undefined,
+  icon: 'circle',
+  indeterminate: false,
+  maxContentRow: 5,
+  maxLabelRow: 3,
+  placement: 'left',
+  readonly: false,
+};
 
 export const checkboxGroupDefaultProps: TdCheckboxGroupProps = {
   disabled: undefined,
--- packages/products/tdesign-mobile-react/src/checkbox/type.ts
+++ packages/products/tdesign-mobile-react/src/checkbox/type.ts
@@ -4,6 +4,100 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
+import { TNode } from '../common';
+import { ChangeEvent } from 'react';
+
+export interface TdCheckboxProps {
+  /**
+   * 是否为块级元素
+   * @default true
+   */
+  block?: boolean;
+  /**
+   * 是否开启无边框模式
+   * @default false
+   */
+  borderless?: boolean;
+  /**
+   * 用于标识是否为「全选选项」。单独使用无效，需在 CheckboxGroup 中使用
+   * @default false
+   */
+  checkAll?: boolean;
+  /**
+   * 是否选中
+   * @default false
+   */
+  checked?: boolean;
+  /**
+   * 是否选中，非受控属性
+   * @default false
+   */
+  defaultChecked?: boolean;
+  /**
+   * 多选框内容，同 label
+   */
+  children?: TNode;
+  /**
+   * 多选框内容
+   */
+  content?: TNode;
+  /**
+   * 是否禁用组件内容（content）触发选中
+   */
+  contentDisabled?: boolean;
+  /**
+   * 是否禁用组件。如果父组件存在 CheckboxGroup，默认值由 CheckboxGroup.disabled 控制。优先级：Checkbox.disabled > CheckboxGroup.disabled > Form.disabled
+   */
+  disabled?: boolean;
+  /**
+   * 自定义选中图标和非选中图标。使用 Array 时表示：[选中态图标，非选中态图标]。使用 String 时，值为 circle 表示填充圆形图标、值为 line 表示描边型图标、值为 rectangle 表示填充矩形图标。
+   * @default 'circle'
+   */
+  icon?: 'circle' | 'line' | 'rectangle' | boolean | Array<TNode | String>;
+  /**
+   * 是否为半选
+   * @default false
+   */
+  indeterminate?: boolean;
+  /**
+   * 主文案
+   */
+  label?: TNode;
+  /**
+   * 内容最大行数限制
+   * @default 5
+   */
+  maxContentRow?: number;
+  /**
+   * 主文案最大行数限制
+   * @default 3
+   */
+  maxLabelRow?: number;
+  /**
+   * HTML 元素原生属性
+   * @default ''
+   */
+  name?: string;
+  /**
+   * 多选框和内容相对位置
+   * @default left
+   */
+  placement?: 'left' | 'right';
+  /**
+   * 只读状态
+   * @default false
+   */
+  readonly?: boolean;
+  /**
+   * 多选框的值
+   */
+  value?: string | number | boolean;
+  /**
+   * 值变化时触发
+   */
+  onChange?: (checked: boolean, context: { e: ChangeEvent<HTMLInputElement> }) => void;
+}
+
 export interface TdCheckboxGroupProps<T = CheckboxGroupValue> {
   /**
    * 是否禁用组件。优先级：Form.disabled < CheckboxGroup.disabled < Checkbox.disabled
