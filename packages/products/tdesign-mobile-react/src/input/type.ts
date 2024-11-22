4d24beb7a0f1f44f50437c1d59467b45a74f4531
黎伟杰 <674416404@qq.com>
2024-11-22 14:38:25 +0800

    feat(input): support spellCheck

--- packages/products/tdesign-mobile-react/src/input/type.ts
+++ packages/products/tdesign-mobile-react/src/input/type.ts
@@ -89,6 +89,11 @@ export interface TdInputProps {
    */
   readonly?: boolean;
   /**
+   * 是否开启拼写检查，HTML5 原生属性，[点击查看详情](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/spellcheck)
+   * @default false
+   */
+  spellCheck?: boolean;
+  /**
    * 输入框状态
    * @default default
    */
