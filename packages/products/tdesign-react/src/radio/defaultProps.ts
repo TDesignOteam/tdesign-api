9698c26194107b366d370489ec0e89661b98903c
HaixingOoO <974758671@qq.com>
2024-12-29 01:30:44 +0800

    feat(Radio): radio-group support theme api

--- packages/products/tdesign-react/src/radio/defaultProps.ts
+++ packages/products/tdesign-react/src/radio/defaultProps.ts
@@ -2,20 +2,13 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdRadioProps, TdRadioGroupProps } from './type';
-
-export const radioDefaultProps: TdRadioProps = {
-  allowUncheck: false,
-  defaultChecked: false,
-  disabled: undefined,
-  readonly: undefined,
-  value: undefined,
-};
+import { TdRadioGroupProps } from './type';
 
 export const radioGroupDefaultProps: TdRadioGroupProps = {
   allowUncheck: false,
   disabled: undefined,
   readonly: undefined,
   size: 'medium',
+  theme: 'radio',
   variant: 'outline',
 };
