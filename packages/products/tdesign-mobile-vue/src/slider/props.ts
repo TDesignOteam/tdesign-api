f720bb8cbd5539df8e50848de4924b508a7a98c8
novlan1 <1576271227@qq.com>
2025-02-08 11:47:56 +0800

    chore: update api

--- packages/products/tdesign-mobile-vue/src/slider/props.ts
+++ packages/products/tdesign-mobile-vue/src/slider/props.ts
@@ -66,8 +66,4 @@ export default {
   },
   /** 滑块值变化时触发 */
   onChange: Function as PropType<TdSliderProps['onChange']>,
-  /** 结束拖动时触发 */
-  onDragend: Function as PropType<TdSliderProps['onDragend']>,
-  /** 开始拖动时触发 */
-  onDragstart: Function as PropType<TdSliderProps['onDragstart']>,
 };
