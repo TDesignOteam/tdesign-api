438329e5da9469582e5652a3dd6970fdabf2970b
novlan1 <1576271227@qq.com>
2025-02-13 15:42:51 +0800

    feat(mobile-datetime-picker): add steps/header/footer props

--- packages/products/tdesign-mobile-vue/src/date-time-picker/type.ts
+++ packages/products/tdesign-mobile-vue/src/date-time-picker/type.ts
@@ -4,7 +4,14 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
+import { TNode } from '../common';
+
 export interface TdDateTimePickerProps {
+  /**
+   * 自动关闭；在确认、取消、点击遮罩层自动关闭，不需要手动设置 visible
+   * @default false
+   */
+  autoClose?: boolean;
   /**
    * 取消按钮文字
    * @default 取消
@@ -24,10 +31,18 @@ export interface TdDateTimePickerProps {
    */
   filter?: (type: TimeModeValues, columns: DateTimePickerColumn) => DateTimePickerColumn;
   /**
+   * 底部内容
+   */
+  footer?: TNode;
+  /**
    * 用于pick、change、confirm事件参数格式化[详细文档](https://day.js.org/docs/en/display/format)
    * @default 'YYYY-MM-DD HH:mm:ss'
    */
   format?: string;
+  /**
+   * 顶部内容
+   */
+  header?: TNode;
   /**
    * year = 年；month = 年月；date = 年月日；hour = 年月日时； minute = 年月日时分；当类型为数组时，第一个值控制年月日，第二个值控制时分秒
    * @default 'date'
@@ -47,13 +62,17 @@ export interface TdDateTimePickerProps {
    */
   start?: string | number;
   /**
+   * 时间间隔步数，示例：`{ minute: 5 }`
+   */
+  steps?: Record<TimeModeValues, number>;
+  /**
    * 标题
    * @default '选择时间'
    */
   title?: string;
   /**
    * 是否使用弹出层包裹
-   * @default true
+   * @default false
    */
   usePopup?: boolean;
   /**
@@ -69,6 +88,11 @@ export interface TdDateTimePickerProps {
    */
   modelValue?: DateValue;
   /**
+   * 是否显示
+   * @default false
+   */
+  visible?: boolean;
+  /**
    * 取消按钮点击时触发
    */
   onCancel?: (context: { e: MouseEvent }) => void;
@@ -76,6 +100,10 @@ export interface TdDateTimePickerProps {
    * value改变时触发
    */
   onChange?: (value: DateValue) => void;
+  /**
+   * 关闭时触发
+   */
+  onClose?: (trigger: TriggerSource) => void;
   /**
    * 确认按钮点击时触发
    */
@@ -98,3 +126,5 @@ export type DateTimePickerMode = TimeModeValues | Array<TimeModeValues>;
 export type TimeModeValues = 'year' | 'month' | 'date' | 'hour' | 'minute' | 'second';
 
 export type DateValue = string | number;
+
+export type TriggerSource = 'overlay' | 'cancel-btn' | 'confirm-btn';
