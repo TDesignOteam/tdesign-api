f047e5b2161c8e6a1d1d905e1c834932f48d116e
novlan1 <1576271227@qq.com>
2025-02-17 18:08:41 +0800

    feat(color-picker): update mobile-vue color-picker props

--- packages/products/tdesign-mobile-vue/src/color-picker/type.ts
+++ packages/products/tdesign-mobile-vue/src/color-picker/type.ts
@@ -19,9 +19,19 @@ export interface TdColorPickerProps {
    */
   clearable?: boolean;
   /**
+   * 是否开启透明通道
+   * @default false
+   */
+  enableAlpha?: boolean;
+  /**
    * 底部插槽，仅在 `usePopup` 为 `true` 时有效
    */
   footer?: TNode;
+  /**
+   * 格式化色值。`enableAlpha` 为真时，`RGBA/HSLA/HSVA` 等值有效
+   * @default RGB
+   */
+  format?: 'RGB' | 'RGBA' | 'HSL' | 'HSLA' | 'HSB' | 'HSV' | 'HSVA' | 'HEX' | 'CMYK' | 'CSS';
   /**
    * 顶部插槽，仅在 `usePopup` 为 `true` 时有效
    */
@@ -32,19 +42,77 @@ export interface TdColorPickerProps {
    */
   popupProps?: PopupProps;
   /**
+   * 系统预设的颜色样例，值为 `null` 或 `[]` 则不显示系统色，值为 `undefined` 会显示组件内置的系统默认色
+   */
+  swatchColors?: Array<string> | null;
+  /**
+   * 颜色选择器类型。（base 表示仅展示系统预设内容; multiple 表示展示色板和系统预设内容
+   * @default base
+   */
+  type?: TypeEnum;
+  /**
    * 是否使用弹出层包裹颜色选择器
    * @default false
    */
   usePopup?: boolean;
+  /**
+   * 色值
+   * @default ''
+   */
+  value?: string;
+  /**
+   * 色值，非受控属性
+   * @default ''
+   */
+  defaultValue?: string;
+  /**
+   * 色值
+   * @default ''
+   */
+  modelValue?: string;
   /**
    * 是否显示颜色选择器。`usePopup` 为 true 时有效
    * @default false
    */
   visible?: boolean;
   /**
+   * 选中的色值发生变化时触发，第一个参数 `value` 表示新色值，`context.color` 表示当前调色板控制器的色值，`context.trigger` 表示触发颜色变化的来源
+   */
+  onChange?: (value: string, context: { color: ColorObject; trigger: ColorPickerChangeTrigger }) => void;
+  /**
    * 关闭按钮时触发
    */
   onClose?: (trigger: ColorPickerTrigger) => void;
+  /**
+   * 调色板控制器的值变化时触发，`context.color` 指调色板控制器的值
+   */
+  onPaletteBarChange?: (context: { color: ColorObject }) => void;
 }
 
+export type TypeEnum = 'base' | 'multiple';
+
+export type ColorPickerChangeTrigger =
+  | 'palette-saturation-brightness'
+  | 'palette-saturation'
+  | 'palette-brightness'
+  | 'palette-hue-bar'
+  | 'palette-alpha-bar'
+  | 'input'
+  | 'preset'
+  | 'recent';
+
 export type ColorPickerTrigger = 'overlay';
+
+export interface ColorObject {
+  alpha: number;
+  css: string;
+  hex: string;
+  hex8: string;
+  hsl: string;
+  hsla: string;
+  hsv: string;
+  hsva: string;
+  rgb: string;
+  rgba: string;
+  value: number;
+}
