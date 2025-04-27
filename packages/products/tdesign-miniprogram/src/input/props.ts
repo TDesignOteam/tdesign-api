6dd7961316acd5bf791179df77d59376f74025fc
anlyyao <anly_yaw@163.com>
2025-04-27 15:02:19 +0800

    feat(Input): add extra slot

--- packages/products/tdesign-miniprogram/src/input/props.ts
+++ packages/products/tdesign-miniprogram/src/input/props.ts
@@ -16,6 +16,11 @@ const props: TdInputProps = {
     type: String,
     value: 'left',
   },
+  /** 超出 `maxlength` 或 `maxcharacter` 之后是否允许继续输入 */
+  allowInputOverMax: {
+    type: Boolean,
+    value: false,
+  },
   /** 强制 input 处于同层状态，默认 focus 时 input 会切到非同层状态 (仅在 iOS 下生效) */
   alwaysEmbed: {
     type: Boolean,
