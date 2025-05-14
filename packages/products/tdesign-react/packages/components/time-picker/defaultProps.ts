eca07a669e63d35ced730fd120b37fcce442b5a6
Wesley <985189328@qq.com>
2025-05-14 12:24:31 +0800

    feat: support onConfirm & readonly (#620)
    * feat: support onConfirm & readonly
    
    * chore: rangePicker readonly

--- packages/products/tdesign-react/packages/components/time-picker/defaultProps.ts
+++ packages/products/tdesign-react/packages/components/time-picker/defaultProps.ts
@@ -2,20 +2,7 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdTimePickerProps, TdTimeRangePickerProps } from './type';
-
-export const timePickerDefaultProps: TdTimePickerProps = {
-  allowInput: false,
-  borderless: false,
-  clearable: false,
-  disabled: undefined,
-  format: 'HH:mm:ss',
-  hideDisabledTime: true,
-  placeholder: undefined,
-  size: 'medium',
-  status: 'default',
-  steps: [1, 1, 1],
-};
+import { TdTimeRangePickerProps } from './type';
 
 export const timeRangePickerDefaultProps: TdTimeRangePickerProps = {
   allowInput: false,
@@ -26,6 +13,7 @@ export const timeRangePickerDefaultProps: TdTimeRangePickerProps = {
   format: 'HH:mm:ss',
   hideDisabledTime: true,
   placeholder: undefined,
+  readonly: undefined,
   size: 'medium',
   status: 'default',
   steps: [1, 1, 1],
