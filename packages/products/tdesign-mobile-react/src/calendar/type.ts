8b69422322665d2607fcbef7fe2ce34438132bd9
novlan1 <1576271227@qq.com>
2025-07-03 11:19:34 +0800

    feat(calendar): 优化scroll参数类型 (#659)

--- packages/products/tdesign-mobile-react/src/calendar/type.ts
+++ packages/products/tdesign-mobile-react/src/calendar/type.ts
@@ -90,7 +90,7 @@ export interface TdCalendarProps {
   /**
    * 滚动时触发
    */
-  onScroll?: (context: { e: Event }) => void;
+  onScroll?: (context: { e: React.UIEvent }) => void;
   /**
    * 点击日期时触发
    */
