aed1d00a3985d96be43d3e36be79183b12c015c6
liweijie <674416404@qq.com>
2025-07-14 17:04:17 +0800

    chore: 适配cnb

--- packages/frontend/src/services/api-server.js
+++ packages/frontend/src/services/api-server.js
@@ -1,12 +1,28 @@
 import axios from 'axios'
+import { get } from 'lodash'
+
 
 const cmpApiInstance = axios.create({
-  baseURL: 'http://127.0.0.1:16001/',
+  
+  baseURL: getBaseURL(),
   headers: {
     'Access-Control-Allow-Origin': '*'
   },
   timeout: 1000
 })
+
+function getBaseURL(){
+  if (import.meta.env.MODE !== 'development') {
+    return '/'
+  }
+  else{
+    if (window.location.host.includes('cnb.run')){
+      return window.location.origin.replace('10000.cnb.run', '16001.cnb.run')
+    }
+    return 'http://127.0.0.1:16001/'
+  }
+  
+}
 
 export { cmpApiInstance }
 
