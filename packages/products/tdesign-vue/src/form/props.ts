0651b0d1b1de962c6956a04b9f029347014c2347
Rylan <rylanbot@qq.com>
2025-10-11 17:39:03 +0800

    chore(form): update `onSubmit` descriptions for clarity

--- packages/products/tdesign-vue/src/form/props.ts
+++ packages/products/tdesign-vue/src/form/props.ts
@@ -114,7 +114,7 @@ export default {
   submitWithWarningMessage: Boolean,
   /** 表单重置时触发 */
   onReset: Function as PropType<TdFormProps['onReset']>,
-  /** 表单提交时触发。其中 `context.validateResult` 表示校验结果，`context.firstError` 表示校验不通过的第一个规则提醒。`context.validateResult` 值为 `true` 表示校验通过；如果校验不通过，`context.validateResult` 值为校验结果列表。<br />【注意】⚠️ 默认情况，输入框按下 Enter 键会自动触发提交事件，如果希望禁用这个默认行为，可以给输入框添加  enter 事件，并在事件中设置 `e.preventDefault()` */
+  /** 表单提交时触发。其中 `context.validateResult` 表示校验结果，`context.firstError` 表示校验不通过的第一个规则提醒。`context.validateResult` 值为 `true` 表示校验通过；如果校验不通过，`context.validateResult` 值为校验结果列表。 */
   onSubmit: Function as PropType<TdFormProps['onSubmit']>,
   /** 校验结束后触发，result 值为 true 表示校验通过；如果校验不通过，result 值为校验结果列表 */
   onValidate: Function as PropType<TdFormProps['onValidate']>,
