9a0af27554392681279279ad1b2cf5dbc52ea533
anlyyao <anly_yaw@163.com>
2025-10-24 18:05:36 +0800

    chore: output docs in sequence

--- packages/scripts/docs/vue.js
+++ packages/scripts/docs/vue.js
@@ -43,6 +43,15 @@ const API_DOC_BLOCKS = {
 // 通用属性
 const COMMON_PROPS = ['externalClasses', 'style', 'customStyle'];
 
+function categoryOrder(res) {
+  const CATEGORY_ORDER = ['Props', 'Events', 'Slots', 'External Classes'];
+  return Object.fromEntries(
+    CATEGORY_ORDER.filter((key) => key in res).map((key) => [
+      key,
+      res[key],
+    ])
+  );;
+}
 // category: props / events / functions / extends / return
 function groupByFieldCategory(framework, componentApi) {
   const result = {};
@@ -69,21 +78,11 @@ function groupByFieldCategory(framework, componentApi) {
       }
     }
     const category = (isExtend || isFunction) ? 'Props' : api.field_category_text;
-    if (result[category]) {
-      result[category].push(api);
-    } else {
-      result[category] = [api];
-    }
+    (result[category] ??= []).push(api);
     // 支持属性同名插槽输出到小程序端组件 API 文档
-    if (isSlot) {
-      if (result['Slots']) {
-          result['Slots'].push(api);
-      } else {
-          result['Slots'] = [api];
-      }
-    }
+    if (isSlot) (result['Slots'] ??= []).push(api);
   });
-  return result;
+  return categoryOrder(result);
 }
 
 function sortSlotsArrExceptFirst(arr) {
@@ -530,3 +529,4 @@ function getVueApiDocs(componentMap, current, framework, globalConfigData, langu
 }
 
 module.exports = getVueApiDocs;
+
