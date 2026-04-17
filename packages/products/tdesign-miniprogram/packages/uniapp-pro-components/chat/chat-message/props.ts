24a09198966aaca614ddbf143debe67c28eca8a0
liweijie0812 <674416404@qq.com>
2026-04-17 15:22:32 +0800

    feat(chat-message): add MessageThinking type (#851)
    * feat(chat-message): add MessageThinking type
    
    * fix: longpress rename message-longpress
    
    * chore: fix format
    
    * chore: rename
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/products/tdesign-miniprogram/packages/uniapp-pro-components/chat/chat-message/props.ts
+++ packages/products/tdesign-miniprogram/packages/uniapp-pro-components/chat/chat-message/props.ts
@@ -19,7 +19,7 @@ export default {
   avatar: {
     type: String,
   },
-  /** 聊天内容组件的属性 */
+  /** 聊天内容组件的属性，支持通过 `thinking` 字段透传 ChatThinking 组件属性 */
   chatContentProps: {
     type: Object,
   },
@@ -75,8 +75,8 @@ export default {
       return ['base', 'outline', 'text'].includes(val);
     },
   },
-  /** null */
-  onLongpress: {
+  /** 长按事件 */
+  onMessageLongpress: {
     type: Function,
     default: () => ({}),
   },
