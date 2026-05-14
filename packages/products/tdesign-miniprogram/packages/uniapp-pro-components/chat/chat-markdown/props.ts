749e4cc96952b6aab1594ce357b1be69caa21b8c
黎伟杰 <674416404@qq.com>
2026-05-14 12:37:41 +0800

    feat(chat-markdown): add streaming api

--- packages/products/tdesign-miniprogram/packages/uniapp-pro-components/chat/chat-markdown/props.ts
+++ packages/products/tdesign-miniprogram/packages/uniapp-pro-components/chat/chat-markdown/props.ts
@@ -16,6 +16,10 @@ export default {
     type: Object,
     default: { gfm: true, pedantic: false, breaks: true },
   },
+  /** 流式输出配置，控制尾部光标的显示与隐藏。尾部光标配置，true 使用默认光标 ▋，传对象可自定义光标字符 */
+  streaming: {
+    type: Object,
+  },
   /** 点击链接时触发 */
   onClick: {
     type: Function,
