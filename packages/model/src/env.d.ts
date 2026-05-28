94f3110deb3ce1d643e8dcf73a55f72a243becae
liweijie <674416404@qq.com>
2026-05-28 23:27:10 +0800

    chore: 移除 lodash-es 的类型声明

--- packages/model/src/env.d.ts
+++ packages/model/src/env.d.ts
@@ -1,16 +1,3 @@
-declare module 'lodash-es' {
-  export function isEmpty(value?: unknown): boolean;
-  export function pick<T extends object, K extends keyof T>(object: T, ...paths: K[]): Pick<T, K>;
-  export function find<T>(collection: T[], predicate?: (item: T) => boolean): T | undefined;
-  export function camelCase(string: string): string;
-  export function kebabCase(string: string): string;
-  export function upperFirst(string: string): string;
-  export function lowerFirst(string: string): string;
-  export function uniq<T>(array: T[]): T[];
-  export function groupBy<T>(collection: T[], iteratee: (item: T) => string): Record<string, T[]>;
-  export function includes(collection: string[], value: string): boolean;
-}
-
 declare module 'sql.js/dist/sql-wasm-debug.js' {
   interface Database {
     prepare(sql: string): Statement;
