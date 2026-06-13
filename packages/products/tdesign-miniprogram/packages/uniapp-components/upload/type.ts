03fe46985300521657dffe5365b4d522f457e991
novlan1 <1576271227@qq.com>
2026-06-13 08:43:11 +0800

    feat: support theme and allowUploadDuplicateFile[uniapp] (#899)

--- packages/products/tdesign-miniprogram/packages/uniapp-components/upload/type.ts
+++ packages/products/tdesign-miniprogram/packages/uniapp-components/upload/type.ts
@@ -17,6 +17,11 @@ export interface TdUploadProps {
    */
   addContent?: string;
   /**
+   * 是否允许重复上传相同文件名的文件
+   * @default false
+   */
+  allowUploadDuplicateFile?: boolean;
+  /**
    * 图片上传配置，视频上传配置，文件上传配置等，包含图片尺寸、图片来源、视频来源、视频拍摄最长时间等。更多细节查看小程序官网。[图片上传](https://developers.weixin.qq.com/miniprogram/dev/api/media/image/wx.chooseImage.html)。[视频上传](https://developers.weixin.qq.com/miniprogram/dev/api/media/video/wx.chooseVideo.html)
    */
   config?: UploadMpConfig;
@@ -154,7 +159,7 @@ export interface UploadFile {
   status: 'loading' | 'reload' | 'failed' | 'done';
 }
 
-export type MediaType = 'image' | 'video';
+export type MediaType = 'image' | 'video' | 'mix';
 
 export interface RequestMethodResponse {
   status: 'success' | 'fail';
