bd1f071f744e7cd46132202361865dd900454ca0
HaixingOoO <974758671@qq.com>
2026-08-02 14:39:59 +0800

    feat(Scrollbar): new add Scrollbar api

---
+++ packages/products/tdesign-react/packages/components/scrollbar/defaultProps.ts
@@ -0,0 +1,14 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * */
+
+import { TdScrollbarProps } from './type';
+
+export const scrollbarDefaultProps: TdScrollbarProps = {
+  autoExpand: true,
+  disabled: false,
+  noResize: false,
+  role: 'scrollbar',
+  thumbMinSize: 20,
+  visibility: 'hover',
+};
